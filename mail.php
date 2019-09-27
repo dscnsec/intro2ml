13ca935466bcc90d73d8b8361d8a86ceebcb2440
Varun Vishal <47146480+varunvishal10@users.noreply.github.com>
2019-09-27 23:18:31 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+    $to = 'varunvishal10@gmail.com';
+    $firstname = $_POST["fname"];
+    $subject= $_POST["subject"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    
+
+
+    $headers = 'From Intro to ML Program' . "\r\n";
+    $headers .= "From: " . $email . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'  '.$laststname.'</td>
+        </tr>
+        <tr><td>Subject: '.$subject.'</td></tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Email: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'The message has been sent.';
+    }else{
+        echo 'Failed';
+    }
+
+?>
